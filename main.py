479f3486f2bcf740a10af325f3d1888b7a4f3b65
Alex Virdee <alex.virdee08@gmail.com>
2023-07-15 14:14:41 -0400

    output path to png-images directory

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import os from pdf2image import convert_from_path
+import os 
+from pdf2image import convert_from_path
 
 def pdf_to_png(input_path, output_path):
     pages = convert_from_path(input_path)
     for i, page in enumerate(pages):
-        page.save(f"{output_path}/page_{i+1}.png", "PNG")
+        global_page_number = pdf_to_png.page_counter + i + 1
+        page.save(f"{output_path}/page_{global_page_number}.png", "PNG")
+        pdf_to_png.page_counter += len(pages)
+
+pdf_to_png.page_counter = 0
 
 def convert_folder(folder_path):
-    output_path = os.path.join(folder_path, 'output')  # Specify the output folder for PNG files
+    output_path = os.path.join(folder_path, 'png-images')  # Specify the output folder
     os.makedirs(output_path, exist_ok=True)
     for filename in os.listdir(folder_path):
         if filename.endswith('.pdf'):
@@ -16,5 +21,3 @@ def convert_folder(folder_path):
 if __name__ == '__main__':
     folder_path = input("Enter the folder path: ")
     convert_folder(folder_path)
-
-    
