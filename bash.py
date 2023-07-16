dc71f8dc7cd405ebc2e0cd4db0a3fbdfe2fe589f
Alex Virdee <alex.virdee08@gmail.com>
2023-07-15 21:38:19 -0400

    Terminal app file reference and remove folder path from main convert function

--- bash.py
+++ bash.py
@@ -1,10 +1,6 @@
+# Use this file to convert without UI client (keeping for reference)
 import os 
-from flask import Flask, request, jsonify
 from pdf2image import convert_from_path
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
 
 def pdf_to_png(input_path, output_path):
     pages = convert_from_path(input_path)
@@ -15,17 +11,14 @@ def pdf_to_png(input_path, output_path):
 
 pdf_to_png.page_counter = 0
 
-@app.route('/convert', methods=['POST'])
-def convert_folder():
-    request_data = request.form # Access the form data from the request
-    folder_path = request_data.get('fodler_path') # Extract the folder_path from the form data
-    
+def convert_folder(folder_path):
+    output_path = os.path.join(folder_path, 'png-images')  # Specify the output folder
+    os.makedirs(output_path, exist_ok=True)
     for filename in os.listdir(folder_path):
         if filename.endswith('.pdf'):
             input_path = os.path.join(folder_path, filename)
             pdf_to_png(input_path, output_path)
 
-    return jsonify({ 'message': 'Conversion successful!' })
-
 if __name__ == '__main__':
-    app.run()
+    folder_path = input("Enter the folder path: ")
+    convert_folder(folder_path)
