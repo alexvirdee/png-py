963ebef1173283d72ab943c815d3fc382b47b6a0
Alex Virdee <alex.virdee08@gmail.com>
2023-07-16 01:54:55 -0400

    Download route and flask connection with client. Working on overwrite issue

--- main.py
+++ main.py
@@ -1,31 +1,73 @@
-import os 
-from flask import Flask, request, jsonify
+import os
+import shutil
+import zipfile
 from pdf2image import convert_from_path
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-def pdf_to_png(input_path, output_path):
-    pages = convert_from_path(input_path)
-    for i, page in enumerate(pages):
-        global_page_number = pdf_to_png.page_counter + i + 1
-        page.save(f"{output_path}/page_{global_page_number}.png", "PNG")
-        pdf_to_png.page_counter += len(pages)
-
-pdf_to_png.page_counter = 0
+def convert_to_png(input_path, output_path):
+    images = convert_from_path(input_path)
+    for i, image in enumerate(images):
+        page_number = i + 1
+        image_path = os.path.join(output_path, f"page_{page_number}.png")
+        image.save(image_path, "PNG")
 
 @app.route('/convert', methods=['POST'])
-def convert_folder():
-    request_data = request.form # Access the form data from the request
-    folder_path = request_data.get('fodler_path') # Extract the folder_path from the form data
+def convert_files():
+    files = request.files.getlist('files')
+    print('files', files)
+    output_path = os.path.join(os.getcwd(), 'converted_to_png')
     
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.pdf'):
-            input_path = os.path.join(folder_path, filename)
-            pdf_to_png(input_path, output_path)
+    # Clean up previous converted files
+    shutil.rmtree(output_path, ignore_errors=True)
+    
+    # Create the output folder
+    os.makedirs(output_path, exist_ok=True)
 
-    return jsonify({ 'message': 'Conversion successful!' })
+    try:
+        for file in files:
+            if file.filename.endswith('.pdf'):
+                input_path = os.path.join(output_path, file.filename)
+                file.save(input_path)
+                convert_to_png(input_path, output_path)
+
+        return jsonify({'message': 'Conversion successful!'})
+
+    except Exception as e:
+        return jsonify({'message': f'Conversion failed: {str(e)}'}), 500
+
+@app.route('/download', methods=['GET'])
+def download_images():
+    output_path = os.path.join(os.getcwd(), 'converted_to_png')
+    zip_filename = os.path.join(os.getcwd(), 'converted_to_png.zip')
+
+    try:
+        with zipfile.ZipFile(zip_filename, 'w') as zipf:
+            files_added = 0
+            for root, dirs, files in os.walk(output_path):
+                for file in files:
+                    if file.endswith('.png'):
+                        file_path = os.path.join(root, file)
+                        arcname = os.path.relpath(file_path, output_path)
+                        zipf.write(file_path, arcname=os.path.join('converted_to_png', arcname))
+                        files_added += 1
+
+            app.logger.debug(f"Files added to ZIP: {files_added}")
+
+        return send_file(zip_filename, as_attachment=True)
+
+    except Exception as e:
+        app.logger.error(f"Error occurred during ZIP creation: {e}")
+
+    finally:
+        if os.path.exists(zip_filename):
+            os.remove(zip_filename)
+
+    return jsonify({'message': 'Failed to create ZIP file.'})
+
 
 if __name__ == '__main__':
     app.run()
